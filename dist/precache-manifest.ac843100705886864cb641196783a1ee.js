177da018cde854a0ac54b7ad86e4ad3cb7b0e72b
Lev <levkach@yandex.ru>
2019-06-02 17:30:24 +0200

    Add dist to master

---
+++ dist/precache-manifest.ac843100705886864cb641196783a1ee.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/word-guess/robots.txt"
+  },
+  {
+    "revision": "19b1fe35e57567843009857df3ba1cdb",
+    "url": "/word-guess/media/wrong.19b1fe35.wav"
+  },
+  {
+    "revision": "af9533498aeea71e7ae1f6d59271a494",
+    "url": "/word-guess/media/gameleave.af953349.wav"
+  },
+  {
+    "revision": "840e7327fbb97cb8f005a65f2fde7450",
+    "url": "/word-guess/media/gameenter.840e7327.wav"
+  },
+  {
+    "revision": "65255d264ffe09ea3c2cc9f466a8584e",
+    "url": "/word-guess/media/correct.65255d26.wav"
+  },
+  {
+    "revision": "84a7bf54e563c7400aaa",
+    "url": "/word-guess/js/chunk-vendors.7cdb501d.js"
+  },
+  {
+    "revision": "2f8a0a7705d1c17adf71",
+    "url": "/word-guess/js/app.eff403b7.js"
+  },
+  {
+    "revision": "a0691ee85a1d455f5760c11316cb1063",
+    "url": "/word-guess/index.html"
+  },
+  {
+    "revision": "df64f36c2ad9ca2cab3e0c16ce335cfa",
+    "url": "/word-guess/img/logo.df64f36c.svg"
+  },
+  {
+    "revision": "2f8a0a7705d1c17adf71",
+    "url": "/word-guess/css/app.08562ba2.css"
+  }
+];
